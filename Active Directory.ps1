05cc291027c5239e5f9efc469c7e31a4d5840ed7
Nano404 <60965743+Nano404@users.noreply.github.com>
2020-02-21 21:22:08 +0100

    Update and rename AD DS Script (3).ps1 to Active Directory.ps1

---
+++ Active Directory.ps1
@@ -0,0 +1,16 @@
+﻿#
+# Windows PowerShell script for AD DS Deployment
+#
+Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest `
+-DomainName "Bellaze.local" `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\AD_DS\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainNetbiosName "BELLAZE" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\AD_DS\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\AD_DS\SYSVOL" `
+-Force:$true
